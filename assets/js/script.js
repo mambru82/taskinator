dd03d07a98ec5f7b0c876b4b107af85995af1b08
mambru <ecastro82@gmail.com>
2020-12-22 17:56:12 -0600

    added task info to the form function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,27 @@
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function() {
+var createTaskHandler = function(event) {
+    //prevents automatic refresh upon submission
     event.preventDefault();
+    //captures the input for the task name
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    //captures the input for the task type 
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+   
+    //create the list item
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
+
+    //create div to hold task info and add to list item
+    var taskInfoEl = document.createElement("div");
+    taskInfoEl.className = "task-info";
+
+    //add HTML content to the div
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+  
+    listItemEl.appendChild(taskInfoEl);
+   //add entire list item to list
     tasksToDoEl.appendChild(listItemEl);
 };
 
